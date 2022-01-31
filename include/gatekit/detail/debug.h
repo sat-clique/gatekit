9e9f99cb0678453794487658087a9172cff4b456
Felix Kutzner <felixkutzner@gmail.com>
2022-01-31 16:54:04 +0000

    adding a header for debug utilities

---
+++ include/gatekit/detail/debug.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#define GATEKIT_DEBUG_WARNING                                                                      \
+  "gatekit/debug.h has been included. This header should only be included for debugging."
+
+#if defined(__GNUG__) || defined(__clang__)
+#pragma message(GATEKIT_DEBUG_WARNING)
+#endif
+
+#include <iostream>
+#include <vector>
+
+namespace gatekit {
+namespace detail {
+
+template <typename T>
+void dump_vector(std::string const& name, std::vector<T> const& clauses)
+{
+  std::cout << name << "=[";
+  for (T const& item : clauses) {
+    std::cout << "( ";
+
+    for (auto lit : iterate(item)) {
+      std::cout << lit << " ";
+    }
+
+    std::cout << ")";
+  }
+  std::cout << "] ";
+}
+
+}
+}
